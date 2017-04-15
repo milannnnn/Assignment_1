2bc816d0ed10fb3f21a36e1f057019399570ed77
Milan Milinkovic <Milan Milinkovic>
2017-04-15 15:47:40 +0200

    Automated Commit to Master Branch

--- git_update_alpha_milan.bat
+++ git_update_alpha_milan.bat
@@ -1,6 +1,6 @@
-git pull origin
+git pull origin master
 git branch alpha_milan
 git checkout alpha_milan
 git add .
 git commit -m "Alpha Commit by Milan"
-git push -u origin master
+git push -u origin alpha_milan
